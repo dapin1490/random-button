122b0c03e83481576ad29e17bb59237a00600f6b
dapin <89961651+dapin1490@users.noreply.github.com>
2024-09-26 00:19:03 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 let genButton;
 let resetButton;
-let isMouseOverButton = false;
+let score;
+let cnt = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -13,19 +14,19 @@ function setup() {
   genButton.position(random(100, windowWidth - 100), random(100, windowHeight - 100));
   // genButton.mouseOver(genFigure);
   genButton.style('background-color', color(random(255), random(255), random(255)));
-  genButton.mouseOver(() => isMouseOverButton = true);
-  genButton.mouseOut(() => isMouseOverButton = false);
+  genButton.mouseReleased(genFigure);
   
   resetButton = createButton(`Reset`);
   resetButton.position(random(100, windowWidth - 100), random(100, windowHeight - 100));
   resetButton.mouseReleased(resetFigure);
   resetButton.style('background-color', color(random(255), random(255), random(255), random(255)));
+  
+  score = createButton(`score: ${cnt}`);
+  score.position(30, 30);
 }
 
 function draw() {
-  if (isMouseOverButton) {
-    genFigure()
-  }
+  score.html(`score: ${cnt}`);
 }
 
 function genFigure() {
@@ -33,12 +34,15 @@ function genFigure() {
   fill(random(0, 256), random(0, 256), random(0, 256), random(0, 256));
   
   ellipse(random(0, width), random(0, height), random(50, 100), random(50, 100));
+  
+  cnt++;
 
   randomButton();
 }
 
 function resetFigure() {
   background(255);
+  cnt = 0;
   randomButton();
 }
 
